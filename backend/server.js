c7f70db16e1cc5113c1ad0fc06521c4dbce04225
kriztiank <kriztiank@gmail.com>
2022-04-30 10:11:57 +0200

    Basic Express Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const express = require('express')
+// const colors = require('colors')
+const dotenv = require('dotenv').config()
+// const { errorHandler } = require('./middleware/errorMiddleware')
+// const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+  res.send('get goals')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
